807e2bdcc62497a1b2c40b3e25c439564d4708e1
Eduardo Cavalcante de Brito <167241764+MrEddie7@users.noreply.github.com>
2024-12-20 20:35:19 -0300

    Add files via upload

---
+++ portifolio_DS_AMS-1-main/projeto_1505/src/ExemploUm.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class ExemploUm {
+    
+    public static void main(String[] args) {
+        Scanner ler=new Scanner(System.in);
+     
+       
+        System.out.println("o primeiro valor é: ");
+        int a=ler.nextInt();
+        
+        System.out.println("o segundo valor é:");
+        int b=ler.nextInt();
+        int s=a+b;
+       
+        System.out.println("O resultado é:"+s);
+    }
+    
+}
